2d80e54fdf6c799a4bee561733e5c0630c7e4f7a
YYL1999 <2291003927@qq.com>
2019-07-20 15:06:07 +0800

    api

--- src/core/global-api/extend.js
+++ src/core/global-api/extend.js
@@ -3,24 +3,29 @@
 import { ASSET_TYPES } from 'shared/constants'
 import { defineComputed, proxy } from '../instance/state'
 import { extend, mergeOptions, validateComponentName } from '../util/index'
-
+//使用基础 Vue 构造器，创建一个“子类”。参数是一个包含组件选项的对象。
 export function initExtend (Vue: GlobalAPI) {
   /**
    * Each instance constructor, including Vue, has a unique
    * cid. This enables us to create wrapped "child
    * constructors" for prototypal inheritance and cache them.
    */
+  //为每一个Vue(包括子类)创建一个序号
   Vue.cid = 0
   let cid = 1
 
   /**
    * Class inheritance
    */
+  
   Vue.extend = function (extendOptions: Object): Function {
+    //创建子类的副本
     extendOptions = extendOptions || {}
+    //获得父级Vue
     const Super = this
     const SuperId = Super.cid
     const cachedCtors = extendOptions._Ctor || (extendOptions._Ctor = {})
+    //如果该子类已经创建 则返回该子类的ID
     if (cachedCtors[SuperId]) {
       return cachedCtors[SuperId]
     }
@@ -29,13 +34,14 @@ export function initExtend (Vue: GlobalAPI) {
     if (process.env.NODE_ENV !== 'production' && name) {
       validateComponentName(name)
     }
-
+    //对象继承，并赋值id
     const Sub = function VueComponent (options) {
       this._init(options)
     }
     Sub.prototype = Object.create(Super.prototype)
     Sub.prototype.constructor = Sub
     Sub.cid = cid++
+    //将父级vue和子类合并
     Sub.options = mergeOptions(
       Super.options,
       extendOptions
@@ -74,12 +80,12 @@ export function initExtend (Vue: GlobalAPI) {
     Sub.extendOptions = extendOptions
     Sub.sealedOptions = extend({}, Sub.options)
 
-    // cache constructor
+    //返回合并后的父子类
     cachedCtors[SuperId] = Sub
     return Sub
   }
 }
-
+//对象劫持初始化props
 function initProps (Comp) {
   const props = Comp.options.props
   for (const key in props) {
